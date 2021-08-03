af4435d236acb2bc7c06e9dbf43ae5c6e011b181
Lewis Lakerink <llakerink@swin.edu.au>
2021-08-03 13:44:48 +1000

    Fix incorrect wsgi module
    Summary:
    N/A
    
    @bypass-review because change is trivial
    
    Test Plan: N/A
    
    Reviewers: jesmigel
    
    Differential Revision: https://phab.adacs.org.au/D629

--- runserver.sh
+++ runserver.sh
@@ -2,4 +2,4 @@
 /src/venv/bin/python /src/production-manage.py migrate;
 /src/venv/bin/python /src/production-manage.py collectstatic --noinput;
 
-/src/venv/bin/gunicorn gw_compasui.wsgi -b 0.0.0.0:8000
+/src/venv/bin/gunicorn gw_compas.wsgi -b 0.0.0.0:8000
