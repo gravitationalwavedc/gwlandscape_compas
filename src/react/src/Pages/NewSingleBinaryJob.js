08b159ac9c255b9fac613636220eda473deb27aa
Eman Ali <emanehab99@gmail.com>
2022-04-20 11:07:56 +1000

    To fix plot URLs in production, added an env check to point to localhost if it's dev env, otherwise point to production server

--- src/react/src/Pages/NewSingleBinaryJob.js
+++ src/react/src/Pages/NewSingleBinaryJob.js
@@ -37,6 +37,10 @@ const checkFileExist = (urlToFile) => {
     return (xhr.status != '404')? true : false;
 };
 
+
+const IS_DEV = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+const server_url = IS_DEV ? "http://localhost:8003" : "https://gwlandscape.org.au";
+
 const NewSingleBinaryJob = ({initialValues, router, ...props}) => {
 
     const formik = useFormik({
@@ -120,18 +124,18 @@ const NewSingleBinaryJob = ({initialValues, router, ...props}) => {
                     // console.log(response);
 
                     const myinterval = setInterval(() => {
-                        if((!vanPlotLoaded) && checkFileExist('http://localhost:8003' + response.newSingleBinary.result.vanPlotFilePath)){
-                            setVanPlotFile('http://localhost:8003' + response.newSingleBinary.result.vanPlotFilePath);
+                        if((!vanPlotLoaded) && checkFileExist(server_url + response.newSingleBinary.result.vanPlotFilePath)){
+                            setVanPlotFile(server_url + response.newSingleBinary.result.vanPlotFilePath);
                             setVanPlotLoaded(true);
                         }
 
-                        if((!detailedPlotLoaded) && checkFileExist('http://localhost:8003' + response.newSingleBinary.result.plotFilePath)){
-                            setPlotFile('http://localhost:8003' + response.newSingleBinary.result.plotFilePath);
+                        if((!detailedPlotLoaded) && checkFileExist(server_url + response.newSingleBinary.result.plotFilePath)){
+                            setPlotFile(server_url + response.newSingleBinary.result.plotFilePath);
                             setDetailedPlotLoaded(true);
                         }
 
                         // setGridFile('http://localhost:8003' + response.newSingleBinary.result.gridFilePath);
-                        setDetailedOutputFile('http://localhost:8003' + response.newSingleBinary.result.detailedOutputFilePath);
+                        setDetailedOutputFile(server_url + response.newSingleBinary.result.detailedOutputFilePath);
                     }, 2000);
 
                     if(vanPlotLoaded && detailedPlotLoaded){
