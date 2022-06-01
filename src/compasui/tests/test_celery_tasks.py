c6ed6997ad148240bd9bc24cd3436523023958a3
Eman Ali <emanehab99@gmail.com>
2022-06-01 10:20:36 +1000

    removing print statements from tests

--- src/compasui/tests/test_celery_tasks.py
+++ src/compasui/tests/test_celery_tasks.py
@@ -13,7 +13,6 @@ class TestCeleryTasks(TestCase):
         self.detailed_output_path = "./compasui/tests/test_data/BSE_Detailed_Output_0.h5"
 
     def test_run_compas_success(self):
-        print("Test run_compas task success")
         with TemporaryDirectory() as output_dir:
             output_path = output_dir
             detailed_output_path = os.path.join(output_path, 'COMPAS_Output',
@@ -23,7 +22,6 @@ class TestCeleryTasks(TestCase):
 
     @patch("compasui.tasks.run_compas_cmd")
     def test_run_compas_failure(self, run_compas_cmd):
-        print("Test run_compas task failure")
         run_compas_cmd.side_effect = Exception('something went wrong')
         with TemporaryDirectory() as output_path:
             detailed_output_path = os.path.join(output_path, 'COMPAS_Output',
@@ -33,7 +31,6 @@ class TestCeleryTasks(TestCase):
 
     @patch("compasui.tasks.run_compas_cmd")
     def test_run_compas_timeout(self, run_compas_cmd):
-        print("Test run_compas task timeout")
         run_compas_cmd.side_effect = SoftTimeLimitExceeded
         with TemporaryDirectory() as output_path:
             detailed_output_path = os.path.join(output_path, 'COMPAS_Output',
@@ -42,7 +39,6 @@ class TestCeleryTasks(TestCase):
             self.assertEqual(result, TASK_TIMEOUT)
 
     def test_run_detailed_evol_plotting_success(self):
-        print("Test run_detailed_evol_plotting success")
         with TemporaryDirectory() as output_dir:
             detailed_plot_path = os.path.join(output_dir, 'detailedEvolutionPlot.png')
             vanDenHeuval_plot_path = os.path.join(output_dir, 'vanDenHeuvalPlot.png')
@@ -53,7 +49,6 @@ class TestCeleryTasks(TestCase):
 
     @patch("compasui.tasks.plotting_main")
     def test_run_detailed_evol_plotting_failure(self, plotting_main):
-        print("Test run_detailed_evol_plotting failure")
         with TemporaryDirectory() as output_dir:
             detailed_plot_path = os.path.join(output_dir, 'detailedEvolutionPlot.png')
             vanDenHeuval_plot_path = os.path.join(output_dir, 'vanDenHeuvalPlot.png')
@@ -65,7 +60,6 @@ class TestCeleryTasks(TestCase):
 
     @patch("compasui.tasks.plotting_main")
     def test_run_detailed_evol_plotting_timeout(self, plotting_main):
-        print("Test run_detailed_evol_plotting timeout")
         with TemporaryDirectory() as output_dir:
             detailed_plot_path = os.path.join(output_dir, 'detailedEvolutionPlot.png')
             vanDenHeuval_plot_path = os.path.join(output_dir, 'vanDenHeuvalPlot.png')
@@ -76,7 +70,6 @@ class TestCeleryTasks(TestCase):
             self.assertEqual(result, TASK_TIMEOUT)
 
     def test_run_detailed_evol_plotting_failure_when_run_compas_fails(self):
-        print("Test run_detailed_evol_plotting failure after run_compas task failure or timewout")
         with TemporaryDirectory() as output_dir:
             detailed_plot_path = os.path.join(output_dir, 'detailedEvolutionPlot.png')
             vanDenHeuval_plot_path = os.path.join(output_dir, 'vanDenHeuvalPlot.png')
