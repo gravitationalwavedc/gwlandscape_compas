914ce5b4c4c2f67a9af6976f10b4effc0f1f8dbc
Eman Ali <emanehab99@gmail.com>
2022-06-06 11:41:28 +1000

    code quality

--- src/compasui/tests/test_single_binary_job_model.py
+++ src/compasui/tests/test_single_binary_job_model.py
@@ -25,17 +25,19 @@ class TestSingleBinaryJobModel(testcases.TestCase):
             separation=0.1
         )
         expected_gridfiletext = "--initial-mass-1 1.5 --initial-mass-2 1.51 --metallicity 0.02 --eccentricity 0.1 " \
-                            "--semi-major-axis 0.1 --kick-magnitude-1 0.0 --kick-magnitude-2 0.0 " \
-                            "--common-envelope-alpha 1.0 --common-envelope-lambda-prescription LAMBDA_NANJING " \
-                            "--common-envelope-lambda 0.1 --remnant-mass-prescription FRYER2012 " \
-                            "--fryer-supernova-engine DELAYED --black-hole-kicks FALLBACK " \
-                            "--kick-magnitude-sigma-CCSN-NS 250.0 --kick-magnitude-sigma-CCSN-BH 256.0 " \
-                            "--kick-magnitude-sigma-ECSN 30.0 --kick-magnitude-sigma-USSN 30.0 --pisn-lower-limit 60.0 " \
-                            "--pisn-upper-limit 135.0 --ppi-lower-limit 35.0 --ppi-upper-limit 60.0 " \
-                            "--pulsational-pair-instability-prescription MARCHANT --maximum-neutron-star-mass 3.0 " \
-                            "--mass-transfer-angular-momentum-loss-prescription ISOTROPIC " \
-                            "--mass-transfer-accretion-efficiency-prescription THERMAL --mass-transfer-fa 0.5 " \
-                            "--mass-transfer-jloss 1.0 "
+                                "--semi-major-axis 0.1 --kick-magnitude-1 0.0 --kick-magnitude-2 0.0 " \
+                                "--common-envelope-alpha 1.0 --common-envelope-lambda-prescription LAMBDA_NANJING " \
+                                "--common-envelope-lambda 0.1 --remnant-mass-prescription FRYER2012 " \
+                                "--fryer-supernova-engine DELAYED --black-hole-kicks FALLBACK " \
+                                "--kick-magnitude-sigma-CCSN-NS 250.0 --kick-magnitude-sigma-CCSN-BH 256.0 " \
+                                "--kick-magnitude-sigma-ECSN 30.0 --kick-magnitude-sigma-USSN 30.0 " \
+                                "--pisn-lower-limit 60.0 --pisn-upper-limit 135.0 --ppi-lower-limit 35.0 " \
+                                "--ppi-upper-limit 60.0 " \
+                                "--pulsational-pair-instability-prescription MARCHANT " \
+                                "--maximum-neutron-star-mass 3.0 " \
+                                "--mass-transfer-angular-momentum-loss-prescription ISOTROPIC " \
+                                "--mass-transfer-accretion-efficiency-prescription THERMAL --mass-transfer-fa 0.5 " \
+                                "--mass-transfer-jloss 1.0 "
         job.save()
         self.grid_file_is_created_successfully(job.id, expected_gridfiletext)
 
@@ -51,17 +53,19 @@ class TestSingleBinaryJobModel(testcases.TestCase):
         )
 
         expected_gridfiletext = "--initial-mass-1 1.5 --initial-mass-2 1.51 --metallicity 0.02 --eccentricity 0.1 " \
-                            "--semi-major-axis 0.1 --kick-magnitude-1 0.0 --kick-magnitude-2 0.0 " \
-                            "--common-envelope-alpha 1.0 --common-envelope-lambda-prescription LAMBDA_NANJING " \
-                            "--common-envelope-lambda 0.1 --remnant-mass-prescription FRYER2012 " \
-                            "--black-hole-kicks FALLBACK " \
-                            "--kick-magnitude-sigma-CCSN-NS 250.0 --kick-magnitude-sigma-CCSN-BH 256.0 " \
-                            "--kick-magnitude-sigma-ECSN 30.0 --kick-magnitude-sigma-USSN 30.0 --pisn-lower-limit 60.0 " \
-                            "--pisn-upper-limit 135.0 --ppi-lower-limit 35.0 --ppi-upper-limit 60.0 " \
-                            "--pulsational-pair-instability-prescription MARCHANT --maximum-neutron-star-mass 3.0 " \
-                            "--mass-transfer-angular-momentum-loss-prescription ISOTROPIC " \
-                            "--mass-transfer-accretion-efficiency-prescription THERMAL --mass-transfer-fa 0.5 " \
-                            "--mass-transfer-jloss 1.0 "
+                                "--semi-major-axis 0.1 --kick-magnitude-1 0.0 --kick-magnitude-2 0.0 " \
+                                "--common-envelope-alpha 1.0 --common-envelope-lambda-prescription LAMBDA_NANJING " \
+                                "--common-envelope-lambda 0.1 --remnant-mass-prescription FRYER2012 " \
+                                "--black-hole-kicks FALLBACK " \
+                                "--kick-magnitude-sigma-CCSN-NS 250.0 --kick-magnitude-sigma-CCSN-BH 256.0 " \
+                                "--kick-magnitude-sigma-ECSN 30.0 " \
+                                "--kick-magnitude-sigma-USSN 30.0 --pisn-lower-limit 60.0 " \
+                                "--pisn-upper-limit 135.0 --ppi-lower-limit 35.0 --ppi-upper-limit 60.0 " \
+                                "--pulsational-pair-instability-prescription MARCHANT " \
+                                "--maximum-neutron-star-mass 3.0 " \
+                                "--mass-transfer-angular-momentum-loss-prescription ISOTROPIC " \
+                                "--mass-transfer-accretion-efficiency-prescription THERMAL --mass-transfer-fa 0.5 " \
+                                "--mass-transfer-jloss 1.0 "
         job.save()
         self.grid_file_is_created_successfully(job.id, expected_gridfiletext)
 
@@ -76,17 +80,19 @@ class TestSingleBinaryJobModel(testcases.TestCase):
         )
 
         expected_gridfiletext = "--initial-mass-1 1.5 --initial-mass-2 1.51 --metallicity 0.02 --eccentricity 0.1 " \
-                             "--semi-major-axis 0.1 --orbital-period 0.12 --kick-magnitude-1 0.0 --kick-magnitude-2 0.0 " \
-                             "--common-envelope-alpha 1.0 --common-envelope-lambda-prescription LAMBDA_NANJING " \
-                             "--common-envelope-lambda 0.1 --remnant-mass-prescription FRYER2012 " \
-                             "--fryer-supernova-engine DELAYED --black-hole-kicks FALLBACK " \
-                             "--kick-magnitude-sigma-CCSN-NS 250.0 --kick-magnitude-sigma-CCSN-BH 256.0 " \
-                             "--kick-magnitude-sigma-ECSN 30.0 --kick-magnitude-sigma-USSN 30.0 --pisn-lower-limit 60.0 " \
-                             "--pisn-upper-limit 135.0 --ppi-lower-limit 35.0 --ppi-upper-limit 60.0 " \
-                             "--pulsational-pair-instability-prescription MARCHANT --maximum-neutron-star-mass 3.0 " \
-                             "--mass-transfer-angular-momentum-loss-prescription ISOTROPIC " \
-                             "--mass-transfer-accretion-efficiency-prescription THERMAL --mass-transfer-fa 0.5 " \
-                             "--mass-transfer-jloss 1.0 "
+                                "--semi-major-axis 0.1 --orbital-period 0.12 --kick-magnitude-1 0.0 " \
+                                "--kick-magnitude-2 0.0 --common-envelope-alpha 1.0 " \
+                                "--common-envelope-lambda-prescription LAMBDA_NANJING " \
+                                "--common-envelope-lambda 0.1 --remnant-mass-prescription FRYER2012 " \
+                                "--fryer-supernova-engine DELAYED --black-hole-kicks FALLBACK " \
+                                "--kick-magnitude-sigma-CCSN-NS 250.0 --kick-magnitude-sigma-CCSN-BH 256.0 " \
+                                "--kick-magnitude-sigma-ECSN 30.0 --kick-magnitude-sigma-USSN 30.0 " \
+                                "--pisn-lower-limit 60.0 --pisn-upper-limit 135.0 --ppi-lower-limit 35.0 " \
+                                "--ppi-upper-limit 60.0 " \
+                                "--pulsational-pair-instability-prescription MARCHANT " \
+                                "--maximum-neutron-star-mass 3.0 " \
+                                "--mass-transfer-angular-momentum-loss-prescription ISOTROPIC " \
+                                "--mass-transfer-accretion-efficiency-prescription THERMAL --mass-transfer-fa 0.5 " \
+                                "--mass-transfer-jloss 1.0 "
         job.save()
         self.grid_file_is_created_successfully(job.id, expected_gridfiletext)
-
