d9bac5d9b49eb2e9733ca076369272d7b7a2f486
Eman Ali <emanehab99@gmail.com>
2022-08-23 16:35:00 +1000

    clean up old models, create BasicParameter and advancedParameter model and update CompasJob model

--- src/compasui/admin.py
+++ src/compasui/admin.py
@@ -1,54 +1,28 @@
 from django.contrib import admin
-from .models import CompasJob, DataParameter, Label, SearchParameter, Data, Search, SingleBinaryJob
+from .models import CompasJob, Label, SingleBinaryJob, AdvancedParameter, BasicParameter
 
 
 # Register your models here.
 
 
-class InlineDataAdmin(admin.TabularInline):
-    model = Data
-
-
-class InlineSearchAdmin(admin.TabularInline):
-    model = Search
-
-
-class InlineDataParameterAdmin(admin.TabularInline):
-    model = DataParameter
-
-
-class InlineSearchParameterAdmin(admin.TabularInline):
-    model = SearchParameter
-
-
-@admin.register(Data)
-class DataAdmin(admin.ModelAdmin):
-    fields = ['job', 'data_source', 'source_dataset']
-    inlines = (InlineDataParameterAdmin,)
-
-
-@admin.register(Search)
-class SearchAdmin(admin.ModelAdmin):
-    fields = ['job']
-    inlines = (InlineSearchParameterAdmin,)
-
-
 @admin.register(Label)
 class LabelAdmin(admin.ModelAdmin):
-    fields = ['name', 'description']
+    pass
 
 
 @admin.register(CompasJob)
 class CompasJobAdmin(admin.ModelAdmin):
-    fields = ['name', 'description', 'private', 'job_controller_id', 'labels']
-    filter_horizontal = ('labels',)
-    readonly_fields = ('creation_time', 'last_updated')
-    inlines = (
-        InlineDataAdmin,
-        InlineSearchAdmin,
-        InlineDataParameterAdmin,
-        InlineSearchParameterAdmin
-    )
+    pass
+
+
+@admin.register(AdvancedParameter)
+class AdvancedParameterAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BasicParameter)
+class BasicParameterAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(SingleBinaryJob)
