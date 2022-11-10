c761289e0ab254ebe1c9c5afa712df7558d7f3b1
Eman Ali <emanehab99@gmail.com>
2022-11-10 19:46:09 +1100

    add Kick and Supernova parameter interdependencies

--- src/react/src/Components/Forms/SupernovaKickParametersForm.js
+++ src/react/src/Components/Forms/SupernovaKickParametersForm.js
@@ -8,21 +8,28 @@ import SelectInput from './Atoms/SelectInput';
 
 const SupernovaKickParametersForm = ({formik}) => {
     const [showFryerSupernovaEngine, setShowFryerSupernovaEngine] = useState(true);
+    const [showVelocity, setShowVelocity] = useState(false);
+
 
     const handleRemnantMassPrescriptionOnChange = (e) => {
         formik.setFieldValue('remnantMassPrescription', e.target.value);
         setShowFryerSupernovaEngine(e.target.value === 'FRYER2012');
     };
 
+    const handleKickVelocityDistChange = (e) => {
+        formik.setFieldValue('kickVelocityDistribution', e.target.value);
+        setShowVelocity(e.target.value === 'FIXED');
+    };
+
     return (
         <React.Fragment>
             <Row>
-                <Col>
+                <Col md={8}>
                     <h2>Supernova Remnant and Kick Parameters</h2>
                 </Col>
             </Row>
             <Row>
-                <Col>
+                <Col md={5}>
                     <SelectInput
                         formik={formik}
                         title='Remnant Mass Prescription'
@@ -33,51 +40,50 @@ const SupernovaKickParametersForm = ({formik}) => {
                         onChange={handleRemnantMassPrescriptionOnChange}
                     />
                 </Col>
-                <Col>
+                <Col md={5}>
                     <SelectInput
                         formik={formik}
                         show={showFryerSupernovaEngine}
                         title='Fryer Supernova Engine'
                         name='fryerSupernovaEngine'
                         type='string'
-                        help='Supernova engine type if using the fallback prescription from Fryer et al. (2012)'
+                        help=''
                         options={fryerSupernovaEngineOptions}
                     />
                 </Col>
             </Row>
             <Row>
-                <Col>
+                <Col md={5}>
                     <SelectInput
                         formik={formik}
                         title='Kick Velocity Distribution'
                         name='kickVelocityDistribution'
                         type='string'
-                        help='Natal kick magnitude distribution'
+                        help=''
                         options={kickVelocityDistributionOptions}
+                        onChange={handleKickVelocityDistChange}
                     />
                 </Col>
             </Row>
             <Row>
-                <Col>
+                <Col md={5}>
                     <Input
                         formik={formik}
                         title="Velocity 1 (km/s)"
                         name="velocity1"
                         type="number"
-                        help="Value to be used as the (drawn) kick magnitude for the primary star of a
-                        binary system when evolving in BSE mode, should the star undergo a supernova event
-                        (km s −1 ), Value > 0"
+                        show={showVelocity}
+                        help=""
                     />
                 </Col>
-                <Col>
+                <Col md={5}>
                     <Input
                         formik={formik}
                         title="Velocity 2 (km/s)"
                         name="velocity2"
                         type="number"
-                        help="Value to be used as the (drawn) kick magnitude for the second star of a
-                        binary system when evolving in BSE mode, should the star undergo a supernova event
-                        (km s −1 ), Value > 0"
+                        show={showVelocity}
+                        help=""
                     />
                 </Col>
             </Row>
