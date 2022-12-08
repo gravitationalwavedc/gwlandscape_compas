69fb3e199ae6de487abb0b5d6a225f069b507fe0
Asher <jasherleslie@gmail.com>
2022-12-08 11:17:16 +1100

    Update card to match design.

--- src/react/src/Components/PublicationCard.js
+++ src/react/src/Components/PublicationCard.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 
-const PublicationCard = ({publication}) => <Card>
+const PublicationCard = ({publication}) => <Card className="publication-card">
     <Card.Body>
         <Card.Title className='font-weight-bold'>{publication.title}</Card.Title>
         <Card.Subtitle>
-            {`${publication.author} - ${publication.year}`}
+            {`${publication.author} · ${publication.year}`}
         </Card.Subtitle>
         <Card.Text className='font-italic'>
             {publication.keywords.edges.map(({node}) => node.tag).join(', ')}
         </Card.Text>
+        { publication.datasets ? 
         <Button variant='outline-primary'>
+          Start interactive visualisation
+        </Button> : 
+        <Button variant='outline-primary' disabled>
                 No available models
-        </Button>
+        </Button>}
     </Card.Body>
 </Card>;
 
