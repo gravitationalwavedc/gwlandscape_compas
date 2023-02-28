c631253276264dec42e998a59799de53598c7f7c
emanehab99 <eali@swin.edu.au>
2023-02-28 02:27:30 +0000

    Pass job parameters to COMPAS through YAML config in job client submit function
    - updated bundle submit function to update default compas yaml config using parameter values passed from backend
    - removed as_compas_options (introduced in previous MR) from CompasJob model as it's not relevant there. The mapping from parameter names to compas commands now happens in `submit.py`
    
    Approved-by: Asher <jasherleslie@gmail.com>
    
    Merged by: emanehab99 <eali@swin.edu.au>
    
    See merge request https://gitlab.com/CAS-eResearch/GWDC/code/gwlandscape-compas/-/merge_requests/74

--- src/compasui/models.py
+++ src/compasui/models.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-import traceback
 
 from django.conf import settings
 from django.core.validators import MinValueValidator, MaxValueValidator
@@ -10,7 +9,7 @@ from .utils import constants
 from .utils.jobs.request_file_download_id import request_file_download_id
 from .utils.jobs.request_file_list import request_file_list
 from .utils.jobs.request_job_status import request_job_status
-from .utils.constants import COMPAS_RUN_FIELD_COMMANDS
+# from .utils.constants import COMPAS_RUN_FIELD_COMMANDS
 # from .variables import compas_parameters
 
 
@@ -97,26 +96,6 @@ class CompasJob(models.Model):
             basic=basic,
             advanced=advanced
         )
-
-    def as_compas_options(self):
-        """
-        Reads basic and advanced parameter names, looks up corresponding COMPAS commands
-        then matches the command with parameter value
-        :return:
-        options: dictionary of COMPAS commands and values
-        example: { '--number_of_systems': '100', '--metallicity-distribution': 'ZSOLAR', ...}
-        """
-        options = dict()
-        try:
-            for p in self.basic_parameter.all():
-                options[COMPAS_RUN_FIELD_COMMANDS[p.name]] = p.value
-
-            for p in self.advanced_parameter.all():
-                options[COMPAS_RUN_FIELD_COMMANDS[p.name]] = p.value
-
-            return options
-        except KeyError:
-            traceback.print_exc()
 
     @classmethod
     def get_by_id(cls, bid, user):
