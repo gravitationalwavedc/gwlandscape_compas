2819c9a682df0846ee894645d1ecc72ca430c320
Eman Ali <emanehab99@gmail.com>
2023-03-23 16:15:50 +1100

    show files in a table

--- src/react/src/Components/Results/Files.js
+++ src/react/src/Components/Results/Files.js
@@ -1,39 +1,57 @@
 import React from 'react';
 import {QueryRenderer, graphql} from 'react-relay';
 import {harnessApi} from '../../index';
+import {Table} from 'react-bootstrap';
 
 const Files = ({jobId}) => {
 
     return <React.Fragment>
-        <QueryRenderer
-            environment={harnessApi.getEnvironment('compas')}
-            query={graphql`
-                query FilesQuery ($jobId: ID!) {
-                    compasResultFiles(jobId: $jobId) {
-                        files {
-                            path
-                            downloadToken                          
+        <Table>
+            <thead>
+                <tr>
+                    <th>File Path</th>
+                    <th>Type</th>
+                    <th>File Size</th>
+                </tr>
+            </thead>
+            <tbody>
+                <QueryRenderer
+                    environment={harnessApi.getEnvironment('compas')}
+                    query={graphql`
+                        query FilesQuery ($jobId: ID!) {
+                            compasResultFiles(jobId: $jobId) {
+                                files {
+                                    path
+                                    isDir
+                                    fileSize
+                                    downloadToken                    
+                                }
+                            }
+                        }
+                    `}
+                    variables={{jobId: jobId}}
+                    render={({error, props}) => {
+                        if (error) {
+                            return <tr><td><div>{error.message}</div></td></tr>;
+                        } else if (props && props.compasResultFiles) {
+                            return <React.Fragment>
+                                {
+                                    props.compasResultFiles.files.map(
+                                        (f, i) =>
+                                            <tr key={i}>
+                                                <td>{f.path}</td>
+                                                <td>{f.isDir? 'Directory': 'File'}</td>
+                                                <td>{f.fileSize}</td>
+                                            </tr>
+                                    )
+                                }
+                            </React.Fragment>;
                         }
                     }
-                }
-            `}
-            variables={{jobId: jobId}}
-            render={({error, props}) => {
-                if (error) {
-                    return <div>{error.message}</div>;
-                } else if (props && props.compasResultFiles) {
-                    return <ul>
-                        {
-                            props.compasResultFiles.files.map(
-                                (f, i) => <li key={f.downloadToken}>{f.path}</li>
-                            )
-                        }
-                    </ul>;
-
-                }
-            }
-            }
-        />
+                    }
+                />
+            </tbody>
+        </Table>
     </React.Fragment>;
 };
 
