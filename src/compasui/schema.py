5bab301fac166f9ac4f64fb6a99be3d646c8099d
Eman Ali <emanehab99@gmail.com>
2023-05-17 15:12:00 +1000

    fix user resolver in CompasJobNode

--- src/compasui/schema.py
+++ src/compasui/schema.py
@@ -132,8 +132,9 @@ class CompasJobNode(DjangoObjectType, AbstractBasicParameterType, AbstractAdvanc
                 "data": "Unknown"
             }
 
+    @login_required
     def resolve_user(parent, info):
-        success, users = request_lookup_users([parent.user_id], info.context.user.id)
+        success, users = request_lookup_users([parent.user_id], info.context.user.user_id)
         if success and users:
             return f"{users[0]['firstName']} {users[0]['lastName']}"
         return "Unknown User"
