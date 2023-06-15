7ae361939808701409186f8518af2bcf75caa54f
Eman Ali <emanehab99@gmail.com>
2023-06-15 17:17:46 +1000

    change job status color

--- src/react/src/Pages/MyJobs.js
+++ src/react/src/Pages/MyJobs.js
@@ -5,11 +5,8 @@ import {createFragmentContainer, graphql} from 'react-relay';
 import Link from 'found/Link';
 
 const MyJobs = ({data, match, router}) => {
-<<<<<<< HEAD
-    const jobs = data && data.compasJobs && data.compasJobs.edges.length ? data.compasJobs.edges.map(e => e.node) : null;
-=======
+
     const jobs = data.compasJobs ? data.compasJobs.edges.map(e => e.node) : null;
->>>>>>> added MyJobs page and a route for it
     return (
         <Container>
             <h1 className="pt-5 mb-4">
@@ -32,35 +29,17 @@ const MyJobs = ({data, match, router}) => {
                             </InputGroup>
                         </Form.Group>
                     </Col>
-<<<<<<< HEAD
-                    <Col>
-                        <Link
-                            to={'/compas/job-form'}
-                            exact
-                            match={match}
-                            router={router}
-                            as={Button}
-                            variant='outline-primary'
-                        >New Job</Link>
-                    </Col>
-=======
->>>>>>> added MyJobs page and a route for it
                 </Form.Row>
             </Form>
             {jobs ?
                 jobs.map(job =>
                     <Card key={job.id} className="job-card">
                         <Card.Body>
-<<<<<<< HEAD
-                            <Card.Title data-testid='job-name'>{job.start.name}</Card.Title>
+                            <Card.Title>{job.start.name}</Card.Title>
                             <Card.Subtitle>
                                 <span className={'job-'+job.jobStatus.name.toLowerCase()}>{job.jobStatus.name}</span> .
-                                {job.lastUpdated}
+                                {job.user} . {job.lastUpdated}
                             </Card.Subtitle>
-=======
-                            <Card.Title>{job.start.name}</Card.Title>
-                            <Card.Subtitle>{job.jobStatus.name} . {job.user} . {job.lastUpdated}</Card.Subtitle>
->>>>>>> added MyJobs page and a route for it
                             <Card.Text>{job.start.description}</Card.Text>
                             <Link
                                 to={'/compas/job-results/' + job.id + '/'}
