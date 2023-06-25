4422a74bc76cfadc5859b3e5763d2397c3c30e54
Asher <jasherleslie@gmail.com>
2023-06-26 09:28:39 +1000

    Add CHE to stellarTypes.

--- src/react/src/Components/Plots/VanDenHeuvelUtils.js
+++ src/react/src/Components/Plots/VanDenHeuvelUtils.js
@@ -1,17 +1,53 @@
 /* eslint-disable complexity */
 
 export const eventAlphabet = [
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
 ];
 
 export const stellarTypes = [
     'MS',
     'MS',
-    'HG', 'FGB', 'CHeB', 'EAGB', 'TPAGB', 'HeMS', 'HeHG',
-    'HeGB', 'HeWD', 'COWD', 'ONeWD', 'NS', 'BH', 'MR'
+    'HG',
+    'FGB',
+    'CHeB',
+    'EAGB',
+    'TPAGB',
+    'HeMS',
+    'HeHG',
+    'HeGB',
+    'HeWD',
+    'COWD',
+    'ONeWD',
+    'NS',
+    'BH',
+    'MR',
+    'CHE',
 ];
-
 
 const getEventString = (mtValue) => {
     const mtValueStrings = {
@@ -19,7 +55,7 @@ const getEventString = (mtValue) => {
         2: 'Stable mass transfer from 2 to 1',
         4: `Common envelope initiated by ${Math.floor(mtValue / 2)}`,
         5: 'Double-core common envelope',
-        6: 'Stellar Merger'
+        6: 'Stellar Merger',
     };
 
     try {
@@ -31,11 +67,11 @@ const getEventString = (mtValue) => {
 
 const getImageNumber = (mtValue, stype1, stype2) => {
     const mtValueImageNumbers = {
-        1: (stype2 < 13) ? 26 : 44,
-        2: (stype2 < 13) ? 26 : 44,
-        4: (stype1 < 13 && stype2 < 13) ? 28 : 49,
+        1: stype2 < 13 ? 26 : 44,
+        2: stype2 < 13 ? 26 : 44,
+        4: stype1 < 13 && stype2 < 13 ? 28 : 49,
         5: 28,
-        6: 37
+        6: 37,
     };
 
     try {
@@ -46,7 +82,6 @@ const getImageNumber = (mtValue, stype1, stype2) => {
 };
 
 const getWhichStar = (stype1, stype2, stellarType1, stellarType2) => {
-
     if (stype2 !== stellarType2) {
         return [2, stype2 === 13 || stype2 === 14];
     }
@@ -92,8 +127,9 @@ export const getEvents = (vdhattrData) => {
                 let remType = type_changed_star === 1 ? stype1 : stype2;
                 let comType = type_changed_star === 1 ? stype2 : stype1;
                 let disrupted = vdhattrData.eccentricity[i] > 1 || vdhattrData.semimajor[i] < 0;
-                eventstring = `Star ${type_changed_star} undergoes supernova and forms a ${stellarTypes[remType]} `
-                    + `${disrupted ? '. Orbit becomes unbound' : ''}.`;
+                eventstring =
+                    `Star ${type_changed_star} undergoes supernova and forms a ${stellarTypes[remType]} ` +
+                    `${disrupted ? '. Orbit becomes unbound' : ''}.`;
                 if (disrupted) {
                     if (comType < 13) {
                         image_num = remType === 13 ? 19 : 20;
@@ -119,7 +155,7 @@ export const getEvents = (vdhattrData) => {
         if (i === vdhattrData.time.length - 1 && !isMerger) {
             let eventstring, image_num;
             let isUnbound = vdhattrData.eccentricity[i] > 1 || vdhattrData.semimajor[i] < 0;
-            let types = Array.from({ length: 5 }, (a, b) => b + 10);
+            let types = Array.from({ length: 5 }, (_, b) => b + 10);
 
             let isDCO = types.includes(stype1) && types.includes(stype2);
             if (isDCO) {
@@ -131,11 +167,15 @@ export const getEvents = (vdhattrData) => {
                 let e = vdhattrData.eccentricity[i];
                 let m1 = vdhattrData.mass1[i];
                 let m2 = vdhattrData.mass2[i];
-                let beta = 64 / 5 * G ** 3 * m1 * m2 * (m1 + m2) * Msunkg ** 3 / c ** 5;
+                let beta = ((64 / 5) * G ** 3 * m1 * m2 * (m1 + m2) * Msunkg ** 3) / c ** 5;
                 let T0 = a ** 4 / 4 / beta;
-                let Tdelay = T0 * (1 - e ** 2) ** (7 / 2) * (
-                    1 + 0.31 * e ** 10 + 0.27 * e ** 20 + 0.2 * e ** 1000) / 3.15e7 / 1e6;
-                eventstring = `Double compact object (${stellarTypes[stype1]}+${stellarTypes[stype2]}) merging in ${Tdelay.toFixed(2)} Myr`;
+                let Tdelay =
+                    (T0 * (1 - e ** 2) ** (7 / 2) * (1 + 0.31 * e ** 10 + 0.27 * e ** 20 + 0.2 * e ** 1000)) /
+                    3.15e7 /
+                    1e6;
+                eventstring = `Double compact object (${stellarTypes[stype1]}+${
+                    stellarTypes[stype2]
+                }) merging in ${Tdelay.toFixed(2)} Myr`;
                 if (stype1 === 13 && stype2 === 13) image_num = 55;
                 else if (stype1 === 14 && stype2 === 14) image_num = 51;
                 else image_num = 53;
