55aee87626c542fb8e1c8955d909ba70d6015368
Eman Ali <emanehab99@gmail.com>
2023-08-22 11:12:25 +1000

    clean up JobTitle

--- src/react/src/Components/Forms/JobTitle.js
+++ src/react/src/Components/Forms/JobTitle.js
@@ -1,8 +1,8 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { HiOutlinePencil, HiOutlineCheck, HiOutlineX} from 'react-icons/hi';
 import EdiText from 'react-editext';
 import { useField } from 'formik';
-import {Form} from "react-bootstrap";
+import {Form} from 'react-bootstrap';
 
 const EditButton = () => <React.Fragment><HiOutlinePencil /> edit</React.Fragment>;
 const SaveButton = () => <HiOutlineCheck/>;
@@ -10,13 +10,12 @@ const CancelButton = () => <HiOutlineX/>;
 
 
 const JobTitle = ({formik}) => {
-    const [{ value: nameValue }, { error: nameError }, { setValue: setNameValue }] = useField('name');
-    const [{ value: descriptionValue }, _, { setValue: setDescriptionValue }] = useField('description');
-    const [isChecked, setIsChecked] = useState(formik.values['detailedOutput']);
+    const [{value: nameValue}, {error: nameError}, {setValue: setNameValue}] = useField('name');
+    const [{value: descriptionValue}, _, {setValue: setDescriptionValue}] = useField('description');
 
-    return <React.Fragment>
-        <EdiText 
-            type="text" 
+    <React.Fragment>
+        <EdiText
+            type="text"
             name="name"
             value={nameValue}
             viewProps={{className: 'h2'}}
@@ -24,29 +23,29 @@ const JobTitle = ({formik}) => {
             hint="You can use letters, numbers, underscores, and hyphens."
             editButtonContent={<EditButton/>}
             editButtonClassName="edit-button"
-            saveButtonContent={<SaveButton />}
+            saveButtonContent={<SaveButton/>}
             saveButtonClassName="save-button"
-            cancelButtonContent={<CancelButton />}
+            cancelButtonContent={<CancelButton/>}
             cancelButtonClassName="cancel-button"
             hideIcons
             editOnViewClick
             submitOnUnfocus
             submitOnEnter
         />
-        {nameError && 
-          <small className="text-danger">
-              Invalid name. You can use letters, numbers, underscores, and hyphens.
-          </small>}
-        <EdiText 
-            type="text" 
+        {nameError &&
+            <small className="text-danger">
+                Invalid name. You can use letters, numbers, underscores, and hyphens.
+            </small>}
+        <EdiText
+            type="text"
             name="description"
             value={descriptionValue}
             onSave={(value) => setDescriptionValue(value)}
             editButtonContent={<EditButton/>}
             editButtonClassName="edit-button"
-            saveButtonContent={<SaveButton />}
+            saveButtonContent={<SaveButton/>}
             saveButtonClassName="save-button"
-            cancelButtonContent={<CancelButton />}
+            cancelButtonContent={<CancelButton/>}
             cancelButtonClassName="cancel-button"
             hideIcons
             editOnViewClick
@@ -67,4 +66,5 @@ const JobTitle = ({formik}) => {
 
     </React.Fragment>;
 };
+
 export default JobTitle;
