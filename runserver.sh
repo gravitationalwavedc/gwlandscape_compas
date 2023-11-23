040a2deff4a7512d20d09753010fa6ddad543457
Lewis Lakerink <llakerink@swin.edu.au>
2023-11-23 03:14:19 +0000

    Bump proxy timeouts to one day
    Increase proxy and application timeouts to 1 day to allow large but slow file uploads.
    
    Approved-by: TomReichardt <thomas.reichardt@students.mq.edu.au>
    
    Merged by: Lewis Lakerink <llakerink@swin.edu.au>
    
    See merge request https://gitlab.com/CAS-eResearch/GWDC/code/gwlandscape-compas/-/merge_requests/115

--- runserver.sh
+++ runserver.sh
@@ -2,4 +2,4 @@
 /src/venv/bin/python /src/production-manage.py migrate;
 /src/venv/bin/python /src/production-manage.py collectstatic --noinput;
 
-/src/venv/bin/gunicorn gw_compas.wsgi -b 0.0.0.0:8000 --workers 8 --timeout 600
+/src/venv/bin/gunicorn gw_compas.wsgi -b 0.0.0.0:8000 --workers 8 --timeout 86400
