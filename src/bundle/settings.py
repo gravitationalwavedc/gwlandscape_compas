2eb0029254f1dfd2c3278d48e54b1913e6c3f182
Eman Ali <emanehab99@gmail.com>
2023-11-28 20:50:13 +1100

    change project id in the bundle to the new project

--- src/bundle/settings.py
+++ src/bundle/settings.py
@@ -1,9 +1,9 @@
 # The directory where jobs are stored
-job_directory = "/fred/oz979/GWLandscape/jobs/"
+job_directory = "/fred/oz324/GWLandscape/jobs/"
 
 # Format submission script for specified scheduler.
 scheduler = "slurm"
 
 # Environment scheduler sources during runtime
-scheduler_env = "/fred/oz979/GWLandscape/gwcloud_job_client/bundles/" \
+scheduler_env = "/fred/oz324/GWLandscape/client/bundles/" \
                 "unpacked/05a07631d8efcd1f979e4c4c09fd9fcc4bc9a3a2/env.sh"
