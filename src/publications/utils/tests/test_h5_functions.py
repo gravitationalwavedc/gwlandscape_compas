eb4f4f043c33647318acad24c027085ab5d3a6dc
Tom Reichardt <treichardt@swin.edu.au>
2024-11-13 13:13:31 +1100

    Adding tests for get_h5_subgroup_meta and get_h5_subgroup_data

--- src/publications/utils/tests/test_h5_functions.py
+++ src/publications/utils/tests/test_h5_functions.py
@@ -7,6 +7,7 @@ from django.test import TestCase
 from publications.utils.h5_functions import (
     get_h5_keys,
     get_h5_subgroups,
+    get_h5_subgroup_meta,
     get_h5_subgroup_data,
     get_subgroup_units,
     check_subgroup_boolean,
@@ -33,6 +34,111 @@ class TestGetH5Groups(TestCase):
             self.assertSequenceEqual(get_h5_subgroups(f, 'base_group2'), ['subgroup3', 'subgroup4'])
 
 
+class TestGetH5SubgroupMeta(TestCase):
+    def setUp(self):
+        self.default_subgroups = {
+            'BSE_Common_Envelopes': ['SemiMajorAxis>CE', 'SemiMajorAxis<CE'],
+            'BSE_Double_Compact_Objects': ['Mass(1)', 'Mass(2)'],
+            'BSE_System_Parameters': ['Mass@ZAMS(1)', 'Mass@ZAMS(2)'],
+            'BSE_Supernovae': ['Mass(SN)', 'Mass_CO_Core@CO(SN)']
+        }
+        
+        self.root_groups = ['test_group', *self.default_subgroups.keys()]
+
+        self.subgroups = [
+            'test_dataset_1', 'test_dataset_2', *(val for vals in self.default_subgroups.values() for val in vals)
+        ]
+
+        self.tf = NamedTemporaryFile(suffix='.h5')
+        with h5py.File(self.tf, 'w') as f:
+            for root_group in self.root_groups:
+                for subgroup in self.subgroups:
+                    dataset = f.create_dataset(f'/{root_group}/{subgroup}', data=np.arange(10))
+                    dataset.attrs['units'] = np.bytes_(f'{subgroup}_unit')
+            
+            f.create_dataset('/Run_Details/run_details_dataset', data=np.array([1, 2, 3]))
+
+    def test_returns_default_data_without_kwargs(self):
+        with h5py.File(self.tf, 'w') as f:
+            meta = get_h5_subgroup_meta(f)
+            self.assertSequenceEqual(sorted(meta['groups']), sorted(self.root_groups))
+            self.assertSequenceEqual(sorted(meta['subgroups']), sorted(self.subgroups))
+            self.assertIn(meta['group'], self.root_groups)
+            self.assertEqual(meta['subgroup_x'], self.default_subgroups.get(meta['group'], meta['subgroups'])[0])
+            self.assertEqual(meta['subgroup_y'], self.default_subgroups.get(meta['group'], meta['subgroups'])[1])
+            self.assertEqual(meta['subgroup_x_unit'], f'{meta["subgroup_x"]}_unit')
+            self.assertEqual(meta['subgroup_y_unit'], f'{meta["subgroup_y"]}_unit')
+            self.assertEqual(meta['stride_length'], 1)
+            self.assertEqual(meta['total_length'], 10)
+
+    def test_returns_default_subgroups_for_each_root_group(self):
+        with h5py.File(self.tf, 'w') as f:
+            for root_group in self.default_subgroups.keys():
+                meta = get_h5_subgroup_meta(f, root_group=root_group)
+                self.assertEqual(meta['group'], root_group)
+                self.assertEqual(meta['subgroup_x'], self.default_subgroups[meta['group']][0])
+                self.assertEqual(meta['subgroup_y'], self.default_subgroups[meta['group']][1])
+                self.assertEqual(meta['subgroup_x_unit'], f'{meta["subgroup_x"]}_unit')
+                self.assertEqual(meta['subgroup_y_unit'], f'{meta["subgroup_y"]}_unit')
+
+    def test_returns_first_subgroups_if_unknown_root_group(self):
+        with h5py.File(self.tf, 'w') as f:
+            meta = get_h5_subgroup_meta(f, root_group='test_group')
+            self.assertEqual(meta['group'], 'test_group')
+            self.assertEqual(meta['subgroup_x'], meta['subgroups'][0])
+            self.assertEqual(meta['subgroup_y'], meta['subgroups'][1])
+            self.assertEqual(meta['subgroup_x_unit'], f'{meta["subgroup_x"]}_unit')
+            self.assertEqual(meta['subgroup_y_unit'], f'{meta["subgroup_y"]}_unit')
+
+    def test_returns_subgroups_if_specified(self):
+        with h5py.File(self.tf, 'w') as f:
+            meta = get_h5_subgroup_meta(f, root_group='test_group', subgroup_x='Mass(1)', subgroup_y='Mass(2)')
+            self.assertEqual(meta['group'], 'test_group')
+            self.assertEqual(meta['subgroup_x'], 'Mass(1)')
+            self.assertEqual(meta['subgroup_y'], 'Mass(2)')
+            self.assertEqual(meta['subgroup_x_unit'], f'{meta["subgroup_x"]}_unit')
+            self.assertEqual(meta['subgroup_y_unit'], f'{meta["subgroup_y"]}_unit')
+
+    def test_returns_stride_length_if_specified(self):
+        with h5py.File(self.tf, 'w') as f:
+            meta = get_h5_subgroup_meta(f, stride_length=2)
+            self.assertEqual(meta['stride_length'], 2)
+
+
+class TestGetH5SubgroupData(TestCase):
+    def setUp(self):
+        self.tf = NamedTemporaryFile(suffix='.h5')
+        with h5py.File(self.tf, 'w') as f:
+            test_dataset = f.create_dataset('/base_group/test_dataset', data=np.array([10, 10, 10, 10, 20, 30], dtype=np.uint32))
+            test_dataset.attrs['units'] = np.bytes_('test_unit')
+
+            f.create_dataset('/base_group/string_dataset', data=np.array([b'string_type']))
+
+    def test_returns_none_if_string_type(self):
+        with h5py.File(self.tf, 'w') as f:
+            self.assertIsNone(get_h5_subgroup_data(f, 'base_group', 'string_dataset', 'test_dataset'))
+            self.assertIsNone(get_h5_subgroup_data(f, 'base_group', 'test_dataset', 'string_dataset'))
+
+    def test_returns_data(self):
+        with h5py.File(self.tf, 'w') as f:
+            self.assertDictEqual(
+                get_h5_subgroup_data(f, 'base_group', 'test_dataset', 'test_dataset'),
+                {
+                    'sides': [0.5, 0.5],
+                    'hist_data': '[{"x": 10.0, "y": 10.0, "counts": 4.0}]',
+                    'scatter_data': '[{"x": 20.0, "y": 20.0}, {"x": 30.0, "y": 30.0}]',
+                    'min_max_x': [10.0, 30.0],
+                    'min_max_y': [10.0, 30.0],
+                    'null_check_x': False,
+                    'null_check_y': False,
+                    'log_check_x': False,
+                    'log_check_y': False,
+                    'bool_check_x': False,
+                    'bool_check_y': False
+                }
+            )
+
+
 class TestGetH5SubgroupMethods(TestCase):
     def setUp(self):
         self.tf = NamedTemporaryFile(suffix='.h5')
@@ -56,10 +162,6 @@ class TestGetH5SubgroupMethods(TestCase):
             bool_dataset.attrs['units'] = np.bytes_('State')
 
             f.create_dataset('/base_group/string_dataset', data=np.array([b'string_type']))
-
-    def test_h5_data_returns_none_if_string(self):
-        with h5py.File(self.tf, 'w') as f:
-            self.assertIsNone(get_h5_subgroup_data(f, 'base_group', 'string_dataset', 'int_dataset'))
 
     def test_get_units(self):
         with h5py.File(self.tf, 'w') as f:
