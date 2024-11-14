539324220e334dfe9dc5a57b9c78a20c4d2b52e3
Lewis Lakerink <llakerink@swin.edu.au>
2024-11-14 18:56:58 +0000

    Update docker compose to work as a standalone deployment
    Updates docker compose and other project files as required to allow the project to run standalone on a VM internally on the GWDC network.
    
    Approved-by: emanehab99 <eali@swin.edu.au>
    
    Merged by: Lewis Lakerink <llakerink@swin.edu.au>
    
    See merge request https://gitlab.com/CAS-eResearch/GWDC/gwlandscape-compas/-/merge_requests/121

--- src/gw_compas/production-settings.py
+++ src/gw_compas/production-settings.py
@@ -11,8 +11,8 @@ ALLOWED_HOSTS = ['*']
 EMAIL_HOST = 'mail.swin.edu.au'
 EMAIL_PORT = 25
 
-GWCLOUD_JOB_CONTROLLER_API_URL = "http://adacs-job-controller.jobcontroller.svc.cluster.local:8000/job/apiv1"
-GWCLOUD_AUTH_API_URL = "http://gwcloud-auth:8000/auth/graphql"
+GWCLOUD_JOB_CONTROLLER_API_URL = "http://jobcontroller.adacs.org.au/job/apiv1"
+GWCLOUD_AUTH_API_URL = "http://gwlandscape.org.au/auth/graphql"
 GWCLOUD_DB_SEARCH_API_URL = "http://gwcloud-db-search:8000/graphql"
 CELERY_BROKER_URL = 'redis://gwlandscape-compas-redis:6379'
 CELERY_RESULT_BACKEND = 'redis://gwlandscape-compas-redis:6379'
