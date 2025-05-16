a54fc244038812432b0f645d692faf7cc4be9994
Lewis Lakerink <llakerink@swin.edu.au>
2025-05-16 02:55:12 +0000

    Remove COMPAS requirement for CI/Tests

--- src/compasui/tasks.py
+++ src/compasui/tasks.py
@@ -9,31 +9,40 @@ from .utils.constants import TASK_SUCCESS, TASK_FAIL, TASK_TIMEOUT
 from celery.exceptions import SoftTimeLimitExceeded
 
 
-def check_output_file_generated(outputfilepath):
+def check_output_file_generated(outputfilepath, timeout_seconds=20):
     """
     Check if the job finished successfully by checking that output file is created
-    This will keep running until file is created or timeout otherwise
+    This will keep checking for up to timeout_seconds
     :param outputfilepath: full path of output file
-    :return:
+    :param timeout_seconds: maximum time to wait in seconds
+    :return: TASK_SUCCESS if file exists, TASK_TIMEOUT if it times out
     """
-    created = False
-    # keep checking until output file is generated
-    while not created:
-        created = os.path.exists(outputfilepath)
+    import time
 
-    return TASK_SUCCESS
+    start_time = time.time()
+    while time.time() - start_time < timeout_seconds:
+        if os.path.exists(outputfilepath):
+            return TASK_SUCCESS
+        time.sleep(0.5)  # Check every half second
+
+    # If we get here, we've timed out
+    return TASK_TIMEOUT
 
 
 @shared_task
 def run_compas(parameter_str, output_path):
     result = None
     try:
-        git_directory = os.environ.get('COMPAS_ROOT_DIR')
-        compas_executable = Path(git_directory) / 'src/COMPAS'
-        compas_command = (f'{compas_executable} --detailed-output --number-of-systems 1 '
-                          f'--output-path {output_path} {parameter_str}')
+        git_directory = os.environ.get("COMPAS_ROOT_DIR")
+        compas_executable = Path(git_directory) / "src/COMPAS"
+        compas_command = (
+            f"{compas_executable} --detailed-output --number-of-systems 1 "
+            f"--output-path {output_path} {parameter_str}"
+        )
 
-        detailed_output_file_path = f'{output_path}/COMPAS_Output/Detailed_Output/BSE_Detailed_Output_0.h5'
+        detailed_output_file_path = (
+            f"{output_path}/COMPAS_Output/Detailed_Output/BSE_Detailed_Output_0.h5"
+        )
 
         call(compas_command, shell=True)
         result = check_output_file_generated(detailed_output_file_path)
